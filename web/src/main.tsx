2710cf0a139bc244a1719a126f951278c0dcdf3b
Paulo <prs341450@gmail.com>
2026-08-26 20:17:30 -0300

    Redesign interface as an operational field register

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -9,6 +9,7 @@ import './styles.css';
 import './premium.css';
 import './motion.css';
 import './redesign.css';
+import './field-ui.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
